4794d4f19eb4943cf1ffff1c0d2a9fe245cc16e1
Ponyashka7316 <yuriygurin1@mail.ru>
2019-02-13 12:18:12 +0200

    Improving extention method

--- CSharpTest/DateTimeExtentions.cs
+++ CSharpTest/DateTimeExtentions.cs
@@ -8,9 +8,8 @@ namespace CSharpTest
 {
     public static class DateTimeExtensions
     {
-        public static bool InRange(this DateTime dateToCheck, DateTime startDate, DateTime endDate)
-        {
-            return dateToCheck >= startDate && dateToCheck <= endDate;
-        }
+        public static bool InRange(this DateTime dateToCheck, DateTime startDate, DateTime endDate) =>      
+             dateToCheck >= startDate && dateToCheck <= endDate;
+        
     }
 }

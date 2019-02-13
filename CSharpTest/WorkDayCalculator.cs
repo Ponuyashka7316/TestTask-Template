9567328d75b2189af615b611bd9d7a03459aa262
Ponyashka7316 <yuriygurin1@mail.ru>
2019-02-13 10:48:32 +0200

    Добавьте файлы проекта.

---
+++ CSharpTest/WorkDayCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CSharpTest
+{
+    public class WorkDayCalculator : IWorkDayCalculator
+    {
+        /// <summary>
+        /// Calculating work days
+        /// </summary>
+        /// <param name="startDate">date of commencement of work</param>
+        /// <param name="dayCount">count of work days</param>
+        /// <param name="weekEnds">array of weekend periods</param>
+        /// <returns></returns>
+        public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
+        {
+            DateTime resultDate = startDate;
+            int workDaysCounter = dayCount - 1;
+            while (workDaysCounter != 0)
+            {
+                if (weekEnds != null)
+                    for (int i = 0; i < weekEnds.Length; i++)
+                        if (weekEnds[i].StartDate > weekEnds[i].EndDate)
+                            throw new ArgumentException();
+                        else if (resultDate.InRange(weekEnds[i].StartDate, weekEnds[i].EndDate))
+                        {
+                            ++workDaysCounter;
+                            break;
+                        }
+                resultDate = resultDate.AddDays(1);
+                --workDaysCounter;
+            }
+            return resultDate;
+        }
+    }
+}
---
+++ CSharpTest/WorkDayCalculatorTests.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    [TestClass]
+    public class WorkDayCalculatorTests
+    {
+
+        [TestMethod]
+        public void TestNoWeekEnd()
+        {
+            DateTime startDate = new DateTime(2014, 12, 1);
+            int count = 10;
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, null);
+
+            Assert.AreEqual(startDate.AddDays(count - 1), result);
+        }
+
+        [TestMethod]
+        public void TestNormalPath()
+        {
+            DateTime startDate = new DateTime(2017, 4, 21);
+            int count = 5;
+            WeekEnd[] weekends = new WeekEnd[1]
+            {
+                new WeekEnd(new DateTime(2017, 4, 23), new DateTime(2017, 4, 25))
+            };
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekends);
+
+            Assert.IsTrue(result.Equals(new DateTime(2017, 4, 28)));
+        }
+
+        [TestMethod]
+        public void TestWeekendAfterEnd()
+        {
+            DateTime startDate = new DateTime(2017, 4, 21);
+            int count = 5;
+            WeekEnd[] weekends = new WeekEnd[2]
+            {
+                new WeekEnd(new DateTime(2017, 4, 23), new DateTime(2017, 4, 25)),
+                new WeekEnd(new DateTime(2017, 4, 29), new DateTime(2017, 4, 29))
+            };
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekends);
+
+            Assert.IsTrue(result.Equals(new DateTime(2017, 4, 28)));
+        }
+
+        [TestMethod]
+        public void TestDoubleWeekends()
+        {
+            DateTime startDate = new DateTime(2017, 4, 1);
+            int count = 10;
+            WeekEnd[] weekends = new WeekEnd[2]
+            {
+                new WeekEnd(new DateTime(2017, 4, 2), new DateTime(2017, 4, 4)),
+                new WeekEnd(new DateTime(2017, 4, 6), new DateTime(2017, 4, 7))
+            };
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekends);
+
+            Assert.IsTrue(result.Equals(new DateTime(2017, 4, 15)));
+        }
+
+        [TestMethod]
+        public void TestCurrentDate()
+        {
+            DateTime startDate = DateTime.Now;
+            int count = 12;
+            WeekEnd[] weekends = new WeekEnd[1]
+            {
+                new WeekEnd(DateTime.Now.AddDays(2), DateTime.Now.AddDays(3))
+            };
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekends);
+
+            Assert.AreEqual(startDate.AddDays(count), result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestIncorrectWeekends()
+        {
+            DateTime startDate = new DateTime(2017, 4, 21);
+            int count = 5;
+            WeekEnd[] weekends = new WeekEnd[1]
+            {
+                new WeekEnd(new DateTime(2017, 4, 25), new DateTime(2017, 4, 24))
+            };
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekends);
+
+        }
+    }
+}
